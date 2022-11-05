71e1dfdccad01c481d92bb7cf4239e879a65ec18
Rishabh Gist <damyrachit@gmail.com>
2022-11-05 18:33:10 +0530

    added navigation

--- view-tube/src/app/app.module.ts
+++ view-tube/src/app/app.module.ts
@@ -4,16 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ChipsBarComponent } from './chips-bar/chips-bar.component';
 import { VideoContentComponent } from './video-content/video-content.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     VideoContentComponent,
-    ChipsBarComponent
+    ChipsBarComponent,
+    NavigationBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
